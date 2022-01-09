79f6d1e00b99494a92c979f5baf7e2187e34dc96
hamed ismaeal <hamed.ismaeal1995@gmail.com>
2022-01-10 00:43:49 +0200

    server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+'use strict'
 const express = require('express');
-const app = require('../Api/app');
+const app = express();
 const path = require('path');
 const debug = require('debug')('node-angular');
 const http = require('http');
